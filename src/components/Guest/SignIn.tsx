51ca8cb8c87d5f345c596b6e6908538c2b255bb1
Ilia Kupitman <KupitmanIL@vk.com>
2026-04-25 15:07:27 +0300

    HW 24.04

--- src/components/Guest/SignIn.tsx
+++ src/components/Guest/SignIn.tsx
@@ -1,15 +1,27 @@
 import {useState} from "react";
 import {useAppDispatch} from "../../app/hooks.ts";
-import {fetchUser} from "../../features/api/accountApi.ts";
+import {useLazyFetchUserQuery} from "../../features/api/accountApi.ts";
 import {createToken} from "../../utils/constants.ts";
+import {setToken} from "../../features/token/tokenSlice.ts";
 
 const SignIn = () => {
     const [login, setLogin] = useState('');
     const [password, setPassword] = useState('');
     const dispatch = useAppDispatch();
+    const [fetchUser] = useLazyFetchUserQuery()
 
-    const handleClickSignIn = () => {
-        dispatch(fetchUser(createToken(login, password)));
+    const handleClickSignIn = async () => {
+        const token = createToken(login, password)
+        try {
+            const {error} = await fetchUser(token);
+            if (error) {
+                console.log('Sign in error', error)
+            }else{
+                dispatch(setToken(token))
+            }
+        } catch(e) {
+            console.log('unknown error', e);
+        }
     }
 
     const handleClickClear = () => {
--- src/features/api/accountApi.ts
+++ src/features/api/accountApi.ts
@@ -1,91 +1,53 @@
-import {createAsyncThunk} from "@reduxjs/toolkit";
-import {base_url, createToken} from "../../utils/constants.ts";
+import {base_url} from "../../utils/constants.ts";
 import type {UserProfile, UserRegister, UserUpdate} from "../../utils/types";
-import type {RootState} from "../../app/store.ts";
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 
-export const registerUser = createAsyncThunk(
-    'user/register',
-    async (user: UserRegister) => {
-        const response = await fetch(`${base_url}/account/register`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(user)
+export const accountApi = createApi({
+   reducerPath: 'account',
+   baseQuery: fetchBaseQuery({baseUrl: base_url}),
+   tagTypes: ['user'],
+
+    endpoints: builder => ({
+        registerUser: builder.mutation<UserProfile, UserRegister>({
+            query: user =>({
+                url: '/account/register',
+                method:'POST',
+                body: user
+            })
+        }),
+        fetchUser: builder.query<UserProfile,string>({
+            query: token => ({
+                url: '/account/login',
+                method:'POST',
+                headers:{
+                    Authorization: token
+                }
+            }),
+            providesTags: ["user"],
+        }),
+        updateUser: builder.mutation<UserProfile,({user: UserUpdate, login: string, token:string})>({
+            query: ({user, login, token}) => ({
+                url:`/account/user/${login}`,
+                method: 'PATCH',
+                body: user,
+                headers:{
+                    Authorization: token
+                }
+            }),
+            invalidatesTags: ["user"],
+        }),
+        changePassword: builder.mutation <void,{newPassword:string, token : string}>({
+            query:({newPassword, token}) => ({
+                url: '/account/password',
+                method: 'PATCH',
+                body: {password: newPassword},
+                headers:{
+                    Authorization: token
+                }
+            })
         })
-        if (response.status === 409) {
-            throw new Error(`User with login ${user.login} already exists`)
-        }
-        if (!response.ok) {
-            throw new Error(`Failed to register user ${user.login}`)
-        }
-        const data = await response.json();
-        const token = createToken(user.login, user.password);
-        return {
-            token,
-            user: data
-        }
-    }
-)
+    })
+})
 
-export const fetchUser = createAsyncThunk(
-    'user/fetch',
-    async (token: string) => {
-        const response = await fetch(`${base_url}/account/login`, {
-            method: 'POST',
-            headers: {
-                Authorization: token
-            }
-        })
-        if (response.status === 401) {
-            throw new Error('Invalid credentials')
-        }
-        if (!response.ok) {
-            throw new Error(`Something went wrong`)
-        }
-        const user = await response.json();
-        return {user, token}
-    }
-)
+export const {useFetchUserQuery, useLazyFetchUserQuery, useRegisterUserMutation, useUpdateUserMutation, useChangePasswordMutation } = accountApi
 
-export const updateUser = createAsyncThunk<UserProfile, UserUpdate, { state: RootState }>(
-    'user/update',
-    async (user, {getState}) => {
-        const response = await fetch(`${base_url}/account/user/${getState().user.login}`, {
-            method: 'PATCH',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: getState().token
-            },
-            body: JSON.stringify(user)
-        });
-        if (response.status === 401) {
-            throw new Error('Invalid credentials')
-        }
-        if (!response.ok) {
-            throw new Error(`Something went wrong`)
-        }
-        return await response.json();
-    }
-)
-
-export const changePassword = createAsyncThunk<string, string, { state: RootState }>(
-    'user/password',
-    async (newPassword, {getState}) => {
-        const response = await fetch(`${base_url}/account/password`, {
-            method: 'PATCH',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: getState().token
-            },
-            body: JSON.stringify({password: newPassword})
-        })
-        if (response.status === 401) {
-            throw new Error('Invalid credentials')
-        }
-        if (!response.ok) {
-            throw new Error(`Something went wrong`)
-        }
-        return createToken(getState().user.login, newPassword);
-    }
-)
